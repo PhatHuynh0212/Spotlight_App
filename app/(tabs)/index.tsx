2fa12520a5a89912b70fd40126071c78582627e7
huynhthanhphat <thanhphat02122003@gmail.com>
2025-03-28 20:16:50 +0700

    protect and verify http route with svix and create user in clerk and convex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../../styles/auth.styles";
 import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function Index() {
+  const { signOut } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Link href={"/notifications"}>Feed screen in tabs</Link>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text style={{ color: "white" }}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
